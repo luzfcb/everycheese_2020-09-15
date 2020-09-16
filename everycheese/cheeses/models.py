67133b855fc8e43f2e11a7a836f89134d33acf92
Fabio C. Barrionuevo <fabio.luz@feldroy.com>
2020-09-16 09:28:12 -0700

    First pass at working Add Cheese form

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django_countries.fields import CountryField
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
+from django.urls import reverse
 
 
 class Cheese(TimeStampedModel):
@@ -26,3 +27,9 @@ class Cheese(TimeStampedModel):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        """Return absolute URL to the Cheese Detail page."""
+        return reverse(
+            'cheeses:detail', kwargs={"slug": self.slug}
+        )
